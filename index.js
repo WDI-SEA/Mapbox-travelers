7896aa2c0bc3b1df61e747e4407d04a22a47c96f
009kings <009kings@gmail.com>
2019-04-17 22:36:31 -0700

    initial commit-setup app

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let app = express()
+let ejsLayouts = require('express-ejs-layouts')
+
+app.use(ejsLayouts);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("static"));
+
+app.get('/', (req, res)=>{
+  res.render('home')
+})
+
+app.listen(process.env.PORT || 8000, console.log('🎧 Port 8000 🎧'))
