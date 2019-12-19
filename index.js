4ca8bd78ea0b8a7e4df7b4a8a92dc99cde4fbcf2
William Downey-Schell <Will@Williams-MacBook-Air.local>
2019-12-19 09:00:16 -0800

    added search routes and functionality, back button

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+require('dotenv').config()
 let express = require('express')
-let app = express()
 let ejsLayouts = require('express-ejs-layouts')
+
+let app = express()
 
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("static"));
 
+app.use('/cities', require('./routes/cities'))
+
 app.get('/', (req, res)=>{
   res.render('home')
 })
 
-// app.use('/', require('./routes/cities'))
+app.use('/', require('./routes/cities'))
+
 
 app.listen(process.env.PORT || 8000, console.log('🎧 Port 8000 🎧'))
--- routes/cities.js
+++ routes/cities.js
@@ -2,8 +2,39 @@ require('dotenv').config()
 const express = require('express')
 const mbxClient = require('@mapbox/mapbox-sdk')
 const mbxGeocode = require('@mapbox/mapbox-sdk/services/geocoding')
+const app = express()
 const router = express.Router()
 const db = require('../models')
 
-const mb = mbxClient({ accessToken: MAPBOX_URL})
+const mb = mbxClient({ accessToken: process.env.MAPBOX_URL})
 const geocode = mbxGeocode(mb)
+
+router.get('/search', (req, res) => {
+    res.render('search')
+})
+
+router.get('/results', (req, res) => {
+    geocode.forwardGeocode({
+        query: `${req.query.city}, ${req.query.state}`,
+        types: ['place'],
+        countries: ['us']
+    })
+    .send()
+    .then((result) => {
+        let results = result.body.features.map(result => { //creates array and returns the keys.properties 
+            return {
+                city: result.place_name,
+                lat: result.center[1],
+                long: result.center[0]
+            }
+        })
+        res.render('results', {query: req.query, results})
+    })
+    .catch((err) => {
+        console.log('Error', err)
+    })
+})
+
+
+
+module.exports = router
