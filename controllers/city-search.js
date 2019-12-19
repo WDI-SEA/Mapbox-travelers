ff5f2b794f82144c970d1fa7ab324385b8e4d76a
yboodhan <yboodhan@gmail.com>
2019-12-18 20:50:36 -0800

    added search results page

--- controllers/city-search.js
+++ controllers/city-search.js
@@ -10,4 +10,28 @@ router.get('/', (req, res) => {
     res.render('city-search')
 })
 
+router.get('/search', (req, res) => {
+    //get coordinates by using values in url
+    geocodingClient
+    .forwardGeocode({
+        query: `${req.query.city}, ${req.query.state}`,
+        types: ['place'],
+        countries: ['us']
+    })
+    .send()
+    .then(response => {
+    let results = response.body.features.map (feature => {
+    return {
+        name: feature.place_name,
+        lat: feature.center[1],
+        long: feature.center[0]}
+    })
+    res.render('search-results', { query: req.query, results })
+    })
+    .catch( err => {
+        console.log(err)
+        res.render('404')
+    })
+})
+
 module.exports = router
